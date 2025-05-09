05a1df9f91b7e6712039ff0173ee6d5da8f55dbf
ahincho <ahincho@unsa.edu.pe>
2025-05-09 17:02:36 -0500

    Start Spring Boot Application

---
+++ src/main/java/com/nxtep/loreal/hair/style/HairStyleApplication.java
@@ -0,0 +1,11 @@
+package com.nxtep.loreal.hair.style;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairStyleApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HairStyleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nxtep/loreal/hair/style/HairStyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nxtep.loreal.hair.style;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairStyleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
